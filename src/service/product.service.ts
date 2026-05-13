fa39f06a1a4d4369c7fb2e1a54f31d58e8fb3b46
Iam-Zarif <zarifraja19@gmail.com>
2026-05-14 02:01:35 +0600

    add

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,15 +1,13 @@
-import path from "path"
-const cwd = process.cwd()
-import fs from "fs"
-const filepath = path.join(cwd, "./src/database/db.json")
+import path from "path";
+const cwd = process.cwd();
+import fs from "fs";
+const filepath = path.join(cwd, "./src/database/db.json");
 
-export const readProduct = () =>{
+export const readProduct = () => {
+  const products = fs.readFileSync(filepath, "utf-8");
+  return JSON.parse(products);
+};
 
-    const products = fs.readFileSync(filepath, "utf-8")
-    return JSON.parse(products)
-
-}
-
-export const insertProduct = (payload:unknown) =>{
- fs.writeFileSync(filepath, JSON.stringify(payload))
-}
+export const insertProduct = (payload: unknown) => {
+  fs.writeFileSync(filepath, JSON.stringify(payload));
+};
